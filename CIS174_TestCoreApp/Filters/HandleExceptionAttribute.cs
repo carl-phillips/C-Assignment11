43c4bf1b12897088ef3f12c5656b34febcc831bb
Carl Phillips <36976465+carl-phillips@users.noreply.github.com>
2019-11-06 23:56:06 -0600

    Initial Commit
    New commit demonstrating filters and attributes

---
+++ CIS174_TestCoreApp/Filters/HandleExceptionAttribute.cs
@@ -0,0 +1,36 @@
+﻿using CIS174_TestCoreApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CIS174_TestCoreApp.Filters
+{
+    public class HandleExceptionAttribute : ExceptionFilterAttribute
+    {
+        private readonly ErrorLogContext _errorLogContext;
+        public override void OnException(ExceptionContext context)
+        {
+            var error = new ErrorLog
+            {
+                HttpStatusCode = context.HttpContext.Response.StatusCode,
+                TimeOfError = DateTime.Now,
+                RequestId = Guid.NewGuid(),
+                ExceptionMessage = context.Exception.Message,
+                StackTrace = context.Exception.StackTrace,
+            };
+
+            _errorLogContext.Add(error);
+            _errorLogContext.SaveChanges();
+
+            context.Result = new ObjectResult(error)
+            {
+                StatusCode = error.HttpStatusCode
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ CIS174_TestCoreApp/Startup.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CIS174_TestCoreApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<PersonContext>();
+            services.AddScoped<PersonContext>();
+            services.AddDbContext<ErrorLogContext>();
+            services.AddScoped<ErrorLogContext>();
+            services.AddDbContext<LogContext>();
+            services.AddScoped<LogContext>();
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc(options =>
+            {
+                options.RespectBrowserAcceptHeader = true;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "students",
+                    template: "{controller=Student}/{action=Student}/{accessLevel?}");
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "createperson",
+                    template: "{controller=Student}/{action=Assignment7}");
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "persons",
+                    template: "{controller=Persons}/{action=Index}");
+            });
+            
+        }
+    }
+}
